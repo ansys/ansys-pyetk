0c0dd6332424f07dc6bd7d00803ff4a352618ed0
Jonathan Velasco <jonathan.velasco@ansys.com>
2026-01-09 21:24:24 +0200

    tests: Use UI example to test rectangular cross section

--- tests/backend/tests_backend_api/test_bobbin.py
+++ tests/backend/tests_backend_api/test_bobbin.py
@@ -55,16 +55,6 @@ class TestBackendAPI:
         assert bobbin.volume > 0 and round(abs(bobbin.volume - 110.30603045651762), 4) < vol_limit
 
     def test_create_bobbin_geometry_wound_rectangular(self, aedt_common_fixture_function):
-        json_file_name = "U_wound_rectangular.json"
-        json_path = Path(json_files_path) / json_file_name
-        aedt_common_fixture_function.read_props_from_json(json_path)
-        aedt_common_fixture_function.properties.bobbin.draw_bobbin = True
-
-        bobbin = aedt_common_fixture_function.create_bobbin_geometry()
-
-        assert bobbin.volume > 0 and round(abs(bobbin.volume - 337.62411801165206), 4) < vol_limit
-
-    def test_create_bobbin_geometry_ui_core(self, aedt_common_fixture_function):
         json_file_name = "UI_wound_rectangular.json"
         json_path = Path(json_files_path) / json_file_name
         aedt_common_fixture_function.read_props_from_json(json_path)
