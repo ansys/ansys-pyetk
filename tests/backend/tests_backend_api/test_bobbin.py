7ac2ab46dcf760f577f5e49dfd6ec1e4cb812f8b
Jonathan Velasco <jonathan.velasco@ansys.com>
2026-01-09 17:39:50 +0200

    tests: Improve bobbin test

--- tests/backend/tests_backend_api/test_bobbin.py
+++ tests/backend/tests_backend_api/test_bobbin.py
@@ -27,7 +27,6 @@ pytestmark = [pytest.mark.backend_API]
 
 
 class TestBackendAPI:
-
     def test_create_bobbin_geometry_planar_rectangular(self, aedt_common_fixture_function):
         json_file_name = "EI_planar_rectangular.json"
         json_path = Path(json_files_path) / json_file_name
@@ -44,8 +43,7 @@ class TestBackendAPI:
 
         bobbin = aedt_common_fixture_function.create_bobbin_geometry()
 
-        assert (bobbin.volume > 0 and
-                round(abs(bobbin.volume -1086.5826510971026), 4) < vol_limit)
+        assert bobbin.volume > 0 and round(abs(bobbin.volume - 1086.5826510971026), 4) < vol_limit
 
     def test_create_bobbin_geometry_wound_circular(self, aedt_common_fixture_function):
         json_file_name = "EC_wound_rectangular.json"
@@ -54,8 +52,7 @@ class TestBackendAPI:
 
         bobbin = aedt_common_fixture_function.create_bobbin_geometry()
 
-        assert (bobbin.volume > 0 and
-                round(abs(bobbin.volume - 110.30603045651762), 4) < vol_limit)
+        assert bobbin.volume > 0 and round(abs(bobbin.volume - 110.30603045651762), 4) < vol_limit
 
     def test_create_bobbin_geometry_wound_rectangular(self, aedt_common_fixture_function):
         json_file_name = "U_wound_rectangular.json"
@@ -65,16 +62,4 @@ class TestBackendAPI:
 
         bobbin = aedt_common_fixture_function.create_bobbin_geometry()
 
-        assert (bobbin.volume > 0 and
-                round(abs(bobbin.volume - 337.62411801165206), 4) < vol_limit)
-
-    def test_create_bobbin_geometry_wound_rectangular_ecore(self, aedt_common_fixture_function):
-        json_file_name = "E_wound_rectangular.json"
-        json_path = Path(json_files_path) / json_file_name
-        aedt_common_fixture_function.read_props_from_json(json_path)
-        aedt_common_fixture_function.properties.bobbin.draw_bobbin = True
-
-        bobbin = aedt_common_fixture_function.create_bobbin_geometry()
-
-        assert (bobbin.volume > 0 and
-                round(abs(bobbin.volume - 422.8865923767842), 4) < vol_limit)
+        assert bobbin.volume > 0 and round(abs(bobbin.volume - 337.62411801165206), 4) < vol_limit
