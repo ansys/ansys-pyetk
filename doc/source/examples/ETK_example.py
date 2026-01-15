bed7702e75bb10fd386c410ae77d8357dd39cc96
Sébastien Morais <sebastien.morais@ansys.com>
2026-01-15 11:33:40 +0100

    DOCS: Update example format

--- doc/source/examples/ETK_example.py
+++ doc/source/examples/ETK_example.py
@@ -1,16 +1,33 @@
-"""PyETK Example.
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-This example shows how to use the ``ToolkitBackend`` API to create a transformer model in AEDT.
+# # Use the ToolkitBackend API to create a transformer model in AEDT
+#
+# This example demonstrates how to use the ``ToolkitBackend`` class.
+# More specifically, it does the following:
+# - Initializes the ``ToolkitBackend`` class.
+# - Sets common properties like ``non_graphical`` and ``aedt_version``.
+# - Defines the path to the JSON file with the transformer properties.
+# - Reads the properties from the JSON file.
+# - Launches AEDT.
+# - Creates the transformer model.
 
-The example does the following:
-- Initializes the ``ToolkitBackend`` class.
-- Sets common properties like ``non_graphical`` and ``aedt_version``.
-- Defines the path to the JSON file with the transformer properties.
-- Reads the properties from the JSON file.
-- Launches AEDT.
-- Creates the transformer model.
-
-"""
+# ## Perform required imports
 
 from pathlib import Path
 import sys
@@ -19,14 +36,14 @@ import tempfile
 from ansys.aedt.core import generate_unique_project_name
 from ansys.aedt.toolkits.electronic_transformer.backend.api import ToolkitBackend
 
-# Backend instance
+# ## Create backend instance
 toolkit_api = ToolkitBackend()
 
-# Sets common properties
+# ## Set common properties
 toolkit_api.properties.non_graphical = False
 toolkit_api.properties.aedt_version = "2025.2"
 
-# Defines the Json path
+# ## Define the JSON path
 json_file_name = "RM_wound_circular.json"
 
 temp_dir = tempfile.TemporaryDirectory(suffix="_ansys")
@@ -34,15 +51,15 @@ project_name = generate_unique_project_name(root_name=temp_dir.name, project_nam
 json_path = Path(__file__).parents[3]
 json_path = Path(json_path) / "tests" / "backend" / "json_files" / json_file_name
 
-# Sets the Json reading properties
+# ## Set JSON reading properties
 toolkit_api.read_props_from_json(json_path)
 
-# Launches AEDT
+# ## Launch AEDT
 thread_msg = toolkit_api.launch_thread(toolkit_api.launch_aedt)
 idle = toolkit_api.wait_to_be_idle()
 if not idle:
     print("AEDT not initialized.")
     sys.exit()
 
-# Creates the models
+# ## Create the models
 setup = toolkit_api.create_model()
